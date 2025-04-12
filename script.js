18b986ebdeb945a05b8410457c320a4af2c025a4
PepperMarioYT <142949229+PepperMarioYT@users.noreply.github.com>
2025-04-12 19:10:30 -0400

    better stuff

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const amaForm = document.getElementById('ama-form');
     const amaQuestion = document.getElementById('ama-question');
     const amaResponse = document.getElementById('ama-response');
-    
-        alert("The AMA revamp is being worked on, in the meantime DM the question to me")
+
+    // Handle AMA Form submission
+    amaForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert("The AMA revamp is being worked on, in the meantime DM the question to me");
     });
+});
